c8e6edf2fad5da3ab5c1eb09c9ad94e2b090bd6e
Tom Lorentzen <tom@quarkgammon.com>
2013-01-20 11:45:21 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'da1682849f1926903ae606d8196c5e87161e526882d772dcfdff512ab5e7b7af37051662a4601e2d65b94f738eadd3abc70850fc07fb7ad56a6bfd67506a18f4'
