3d21127d0e827608c7498d2052fd020ada66cb53
Tom Lorentzen <tom@quarkgammon.com>
2013-01-21 00:02:40 -0500

    set html

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static Pages" do
   describe "Home page" do
     it "should have the content: Sample App" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
