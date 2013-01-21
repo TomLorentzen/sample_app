630377f3fb3977c1d7794309cda167c643d27040
Tom Lorentzen <tom@quarkgammon.com>
2013-01-21 16:20:38 -0500

    helpers

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,59 +6,15 @@ describe "Static Pages" do
 
 
     describe "Home page" do
+      include Capybara::DSL
 
-        it "should have the h1 'Sample App'" do
-            visit '/static_pages/home'
-            page.should have_selector('h1', :text => 'Sample App')
-      end
       it "should have the base title 'Home'" do
         visit '/static_pages/home'
-        page.should have_selector('title',
-                              :text => "Ruby on Rails Tutorial App")
+        page.should have_selector('title')
 
       end
-      it "should not have a custom page title" do
-        visit '/static_pages/home'
-        page.should_not have_selector('title', :text => '| Home')
-      end
   end
 
-  describe "Help page" do
 
-    it "should have the h1 'Help'" do
-        visit '/static_pages/help'
-        page.should have_selector('h1', :text => 'Help')
-    end
-    it "should have the right title 'Help'" do
-        visit '/static_pages/help'
-        page.should have_selector('title',
-                              :text => "#{base_title} | Help")
-    end
-  end
 
-  describe "About us" do
-
-    it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector("h1", :text => "About Us")
-    end
-    it "should have the right title 'About Us'" do
-        visit '/static_pages/about'
-        page.should have_selector('title',
-                              :text => "#{base_title} | About Us")
-    end
-  end
-
-  describe "Contact" do
-
-    it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => "Contact")
-    end
-    it "should have the right title 'Contact'" do
-        visit '/static_pages/contact'
-        page.should have_selector('title',
-                              :text => "#{base_title} | Contact")
-    end
-  end
 end
